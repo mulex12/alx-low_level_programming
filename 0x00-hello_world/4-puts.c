563bc9f46e1c627c9e2d259a7d831d53e3f1f381
mulex12 <mulex.john@gmail.com>
2022-06-02 15:01:42 -0700

    tak4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
- /**
- * Description: main function prints out a string to stdout.
- * Return: at the end 0 if success.
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-//*********main function***********
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
