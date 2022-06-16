eaef064ff01f6be336e6ccd9da3d5b18d73cc486
mulex12 <mulex.john@gmail.com>
2022-06-16 03:45:17 -0700

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,7 +17,28 @@ char *_strcat(char *dest, char *src)
 		dlen++;
 	}
 
-	for (i = 0; src[i] != 0; i++)
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}	for (i = 0; src[i] != 0; i++)
 	{
 		dest[dlen] = src[i];
 		dlen++;
