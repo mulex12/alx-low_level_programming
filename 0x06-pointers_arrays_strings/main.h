7dc8c4b98a5302861dc09df2aca1137f7124231f
mulex12 <mulex.john@gmail.com>
2022-06-16 03:56:34 -0700

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,25 +2,6 @@
 #include <stdio.h>
 
 /**
-  * _strcat - Concatenates two strings
-  * @dest: The destination string
-  * @src: The source string
-  *
-  * Return: A pointer to the resulting string dest
-  */
-char *_strcat(char *dest, char *src)
-{
-	int dlen = 0, i;
-
-	while (dest[dlen])
-	{
-		dlen++;
-	}
-
-#include "main.h"
-#include <stdio.h>
-
-/**
  * main - check the code
  *
  * Return: Always 0.
@@ -38,12 +19,4 @@ int main(void)
     printf("%s", s2);
     printf("%s", ptr);
     return (0);
-}	for (i = 0; src[i] != 0; i++)
-	{
-		dest[dlen] = src[i];
-		dlen++;
-	}
-
-	dest[dlen] = '\0';
-	return (dest);
 }
