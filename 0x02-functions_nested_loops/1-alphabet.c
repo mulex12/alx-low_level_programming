055db3e5621eb95c0cf57f9f894b438fd6017024
mulex12 <mulex.john@gmail.com>
2022-06-16 21:56:24 -0700

    fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,8 @@
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  */
-void print_alphabet1(void)
-{
+void print_alphabet(void)
+  {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
