014cc9a2549d4bc7695d359707f898b2e4014157
mulex12 <mulex.john@gmail.com>
2022-06-17 02:39:02 -0700

    m3

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 50 fibonacci.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+long int num1 = 0, num2 = 1, num3 = 0;
+for (i = 0; i < 50; i++)
+{
+
+num3 = num2 + num1;
+num1 = num2;
+num2 = num3;
+if (i != 49)
+printf("%ld, ", num3);
+else
+printf("%ld", num3);
+}
+printf("\n");
+return (0);
+}
