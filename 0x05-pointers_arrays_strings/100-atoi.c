7703edb1f6d7aa42687c770b924c23fb6d9dd90a
mulex12 <mulex.john@gmail.com>
2022-06-17 02:55:16 -0700

    fx100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,25 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _atoi - Find and print the first number in a string as an int
- * @s: String to convert to int
- *
- * Return: The int
+ * _atoi - code to convert string to intiger.
+ * @s: pointer char type
+ * Return: Always 0.
  */
 int _atoi(char *s)
 {
-	int i, n, m;
+int negative = 1, i = 0;
+unsigned int numero = 0;
 
-	i = n = m = 0;
-	while (*(s + i) != '\0')
-	{
-		if (*(s + i) == '-')
-			m++;
-		if (*(s + i) >= '0' && *(s + i) <= '9')
-		{
-			while (*(s + i) >= '0' && *(s + i) <= '9')
-			{
-				if (n == 0)
-				{
-					n = (n * 10) + (*(s + i) - '0');
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == '-')
+negative *= -1;
 
-					
+else if (s[i] >= 0 + '0' && s[i] < 10 + '0')
+numero = numero * 10 + (s[i] - '0');
+
+else if (s[i - 1] >= 0 + '0' && s[i - 1] < 10 + '0')
+break;
+}
+
+return (numero *negative);
+}
