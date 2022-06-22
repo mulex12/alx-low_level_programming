d8a113ea37f76356006ae2b3cb0ca508fbd68a9f
mulex12 <mulex.john@gmail.com>
2022-06-22 03:47:38 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * This is the code to print out the string value using recursion
+ *
+ **/
+void _puts_recursion(char *s)
+{
+  if(*s==0)
+    {
+      _putchar("/n");
+      return 0;
+     }
+  _putchar(*s);
+    _puts_recursion(s+1);
+}
