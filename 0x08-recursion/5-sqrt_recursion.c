e3050c530a792f43723c3334b6a1764f4ebdcc27
mulex12 <mulex.john@gmail.com>
2022-06-22 12:33:14 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,28 @@
 #include "main.h"
+
+/**
+ * sqrtmule - Makes possible to evaluate from 1 to n
+ * @i: same number as n
+ * @j: number used  iterate from 1 to n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
+int sqrtmule(int i, int j)
+{
+	if (j * j == i)
+		return (j);
+	else if (j * j > i)
+		return (-1);
+	return (sqrtmule(i, j + 1));
+}
 /**
  *_sqrt_recursion - to return sqrt of n value
  *@n: integer value
  *Return: On succes 1
 */
+
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
-return (n * _sqrt_recursion(n));
+return (sqrtmule(n, 1));
 }
