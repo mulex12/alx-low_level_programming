8d72f5c29b3c6abd895fd65119c0b0a2d28ccc1f
mulex12 <mulex.john@gmail.com>
2022-06-22 09:42:36 -0700

    2

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - used to print n factorial
+ * n: intiger value
+ * Return: 1 on success
+ * return -1 on errror
+*/
+int factorial(int n)
+{
+   if (n < 0)
+    {
+    return (-1);
+    }
+  return (n * factorial(n - 1));
+}
