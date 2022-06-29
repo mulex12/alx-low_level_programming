362b4344329431ce77fe13f8f03b710d8bddd1a0
mulex12 <mulex.john@gmail.com>
2022-06-29 11:26:24 -0700

    mn

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,66 +1,43 @@
-include "main.h"
+#include <stdlib.h>
+#include "main.h"
 
 /**
- * _isspace - check if a character is whitespace
- * @c: the character to check
- *
- * Return: 1 is c is a whitespace character, otherwise 0
- */
-int _isspace(int c)
-{
-	if (c == 0x20 || (c >= 0x09 && c <= 0x0d))
-		return (1);
-	return (0);
-}
+* **strtow - function that splits a string into words.
+* @str : pointer
+* Return: Char
+*/
 
-
-/**
- * strtow - split a string into words
- * @str: a pointer to the string to split
- *
- * Return: NULL if memory allocation fails or if str is NULL or empty (""),
- * otherwise return a pointer to the array of words terminated by a NULL
- */
 char **strtow(char *str)
 {
-	char **words, *pos = str;
-	int w = 0, c;
+	char **d;
+	int i;
+	int j = 0;
+	int con = 0;
 
-	if (!(str && *str))
+	if (str == NULL)
 		return (NULL);
-	do {
-		while (_isspace(*pos))
-			++pos;
-		if (!*pos)
-			break;
-		while (*(++pos) && !_isspace(*pos))
-			;
-	} while (++w, *pos);
-	if (!w)
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] != 32)
+			con++;
+	}
+
+	d = malloc(sizeof(char) * con);
+
+	if (d == NULL)
 		return (NULL);
-	words = (char **) malloc(sizeof(char *) * (w + 1));
-	if (!words)
-		return (NULL);
-	w = 0, pos = str;
-	do {
-		while (_isspace(*pos))
-			++pos;
-		if (!*pos)
-			break;
-		for (str = pos++; *pos && !_isspace(*pos); ++pos)
-			;
-		words[w] = (char *) malloc(sizeof(char) * (pos - str + 1));
-		if (!words[w])
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] != 32)
 		{
-			while (w >  0)
-				free(words[--w]);
-			free(words);
-			return (NULL);
+			*d[j] = str[i];
+			j++;
 		}
-		for (c = 0; str < pos; ++c, ++str)
-			words[w][c] = *str;
-		words[w][c] = '\0';
-	} while (++w, *pos);
-	words[w] = NULL;
-	return (words);
+		else
+		{
+		}
+	}
+	return (d);
 }
