62945a146dcff6cfa94fb0fea5e254a9e1c07121
mulex12 <mulex.john@gmail.com>
2022-06-29 11:05:41 -0700

    mn

--- 0x0B-malloc_free/100-main.c
+++ 0x0B-malloc_free/100-main.c
@@ -27,7 +27,7 @@ int main(void)
 {
 	char **tab;
 
-	tab = strtow("     Muluken Yohannis         #cisfun      ");
+	tab = strtow("Muluken Yohannis #cisfun");
 	if (tab == NULL)
 	{
 		printf("Failed\n");
