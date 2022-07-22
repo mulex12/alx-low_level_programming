dbe80f2ab304c6469db125dbc50be1f3b4a70b33
mulex12 <mulex.john@gmail.com>
2022-07-22 10:21:51 -0700

    fedit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _realloc - an entry point to reallocate old ptr to new
+ * _realloc - realloc old ptr to new
  * @ptr: old one
  * @old_size: old size of pointer
  * @new_size: new size of pointer
@@ -11,7 +11,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
-	char *mypointer;
+	char *babypointer;
 
 	if (old_size == new_size)
 		return (ptr);
@@ -20,16 +20,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-       mypointer = malloc(sizeof(char) * new_size);
-	if (mypointer == NULL)
+	babypointer = malloc(sizeof(char) * new_size);
+	if (babypointer == NULL)
 		return (NULL);
 	if (ptr)
 	{
 		for (i = 0; i < old_size; i++)
 		{
-			mypointer[i] = *((char *)ptr + i);
+			babypointer[i] = *((char *)ptr + i);
 		}
 	}
 	free(ptr);
-	return (mypointer);
+	return (babypointer);
 }
