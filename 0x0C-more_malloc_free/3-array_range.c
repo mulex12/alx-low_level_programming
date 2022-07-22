a3bd61845d1f9818e57a948605a2a082c0bea272
mulex12 <mulex.john@gmail.com>
2022-07-22 11:15:51 -0700

    mj

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * array_range -an entry point to creates an array of integers
+ * array_range -a function that creates an array of integers
  * @min: lower end of values
  * @max: upper end
  * Return: int *
  */
 int *array_range(int min, int max)
 {
-	int *myarray;
+	int *babyarray;
 	int i;
 
 	if (min > max)
 		return (NULL);
-	myarray = malloc(sizeof(int) * (max - min + 1));
-	if (myarray == NULL)
+	babyarray = malloc(sizeof(int) * (max - min + 1));
+	if (babyarray == NULL)
 		return (NULL);
 	for (i = 0; min <= max; i++)
 	{
 		babyarray[i] = min;
 		min++;
 	}
-	return (myarray);
+	return (babyarray);
 
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,3 +5,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *int_calloc(int nmemb, unsigned int size);
+int _strlen(char *s);
+void mult(int *product, char *n1, char *n2, int len1, int len2);
+int is_valid(char *num);
+void err(int status);
