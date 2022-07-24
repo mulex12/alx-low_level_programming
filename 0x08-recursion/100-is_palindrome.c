c11249542598513e06ce055ea1277160a267bb92
mulex12 <mulex.john@gmail.com>
2022-07-24 08:07:53 -0700

    hn

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,41 +1,47 @@
 #include "main.h"
 
-/**
- * _strlen_recursion - function that returns the length of a string
- *
- * @s: parameter defined in main, pointer to memory (string)
- *
- * Return: integer, length of the string
- */
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		return (0);
-	}
-	s++;
-	return (1 + _strlen_recursion(s));
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * is_palindrome - function that returns 1 if a string is a palindrome
- * and 0 if not
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
  *
- * @s: parameter defined in main, pointer to memory (string)
- *
- * Return: 0 or 1
+ * Return: 1 if palindrome, 0 if not
  */
-
-int is_palindrome(char *s)
+int check_pal(char *s, int i, int len)
 {
-	int len;
-
-	len = _strlen_recursion(s);
-	return (tmp3(s, len));
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
-
-/**
- * tmp3 - subfunction for is_palindrome
- *
- * @s: parameter defined in main, pointer to memory (string)
