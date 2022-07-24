448e7dfa440ab89516d9af0267a490d39adb4154
mulex12 <mulex.john@gmail.com>
2022-07-24 08:01:44 -0700

    edit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,13 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int sqrtmule(int i, int j);
+int tmp1(int n, int i);
 int is_prime_number(int n);
-int primeofmule(int m, int f);
-int palofmule(char *m, int sl);
-int palofmule1(char *m, int sl);
+int tmp2(int n, int i, int stop);
 int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
+int tmp3(char *s, int len);
+
+#endif
