c361031fa1d20445de37119740dbd0803f20bc0c
mulex12 <mulex.john@gmail.com>
2022-09-01 06:39:39 -0700

    fg

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,15 +4,15 @@
  * @h: pointer to the list.
  * Return: number of nodes.
  **/
-size_t print_dlistint(const dlistint_t *h)
+//size_t print_dlistint(const dlistint_t *h)
 {
 const dlistint_t *node = h;
-size_t cont = 0;
+size_t l = 0;
 while (node)
 {
 printf("%i\n", node->n);
-cont++;
+l++;
 node = node->next;
 }
-return (cont);
+return (l);
 }
